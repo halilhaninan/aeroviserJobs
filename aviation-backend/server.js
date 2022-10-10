da7657f6fd1be029c336d595b8cec5ee290a47dc
Halilhan İnan <88934162+halilhaninan@users.noreply.github.com>
2022-10-10 22:00:15 +0300

    update

--- aviation-backend/server.js
+++ aviation-backend/server.js
@@ -1,20 +1,18 @@
 const { response } = require("express");
 const express = require("express");
 const dotenv = require("dotenv").config();
-
 const { hataYakalama } = require("./middlewares/errorMiddleware");
 const baglan = require("./config/db");
-
 const PORT = process.env.PORT;
-
 const app = express();
-
 const cors = require("cors");
 
-app.use(cors()); //
-
-app.use(express.urlencoded({ extended: true }));
+//
+app.use(cors());
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 app.use(express.json());
+
+//
 
 app.use("/api/jobs", require("./routes/notRoute"));
 app.use("/api/user", require("./routes/kullaniciRoute"));
@@ -24,7 +22,5 @@ app.use("/api/upload", require("./routes/CvRoute"));
 //
 
 app.use(hataYakalama);
-
 baglan();
-
 app.listen(PORT, () => console.log(`Server ${PORT} online`));
